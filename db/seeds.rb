bd4acde69f9f4d07ddbe0e43facc936dcac6397e
sachinjeevan <sachinjeevan2506@gmail.com>
2018-02-21 10:04:49 +0000

    seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,25 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+10.times do |blog|
+Blog.create!(
+    title: "My Blog Post #{blog}",
+    body: "ent. In ML, Linear Algebra comes up everywhere. Topics such as Principal Component Analysis (PCA), Singular Value Decomposition (SVD), Eigendecomposition of a matrix, LU Decomposition, QR Decomposition/Factorization, Symmetric Matrices, Orthogonalization & Orthonormalization, Matrix Operations, Projections, Eigenvalues & Eigenvectors, Vector Spaces and Norms are needed for understanding the optimization methods used for machine learning. The amazing thing about Linear Algebra is that there are so many online resources. I have always said that the traditional classroom is dying because of the vast amount of resources available on the internet. My favorite Linear Algebra course is the one offered by MIT Courseware (Prof. Gilbert Strang)."
+    )
+end
+puts "10 blog posts created"
+
+5.times do |skill|
+Skill.create!(
+title: "Rails #{skill}",
+percent_utilized: 15
+)
+end
+puts "5 skills created"
+9.times do |portfolio|
+Portfolio.create!(
+title:"Portfolio title: #{portfolio}",
+subtitle:"My great service",
+body:"hai",
+main_image:"http://via.placeholder.com/600x400",
+thumb_image:"http://via.placeholder.com/350x200"
+)
+end
+puts "9 Portfolio items created"
