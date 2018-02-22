56cd12d1874361e039bce6b447d0be6179ec6f6d
sachinjeevan <sachinjeevan2506@gmail.com>
2018-02-22 04:07:06 +0000

    edit

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -21,5 +21,19 @@ def create
     end
 
   end
-
+  def edit
+  @portfolio_items=Portfolio.find(params[:id])
+  end
+def update
+@portfolio_items=Portfolio.find(params[:id])
+    respond_to do |format|
+      if @portfolio_items.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+        format.html { redirect_to portfolios_path, notice: 'Portfolio Item was successfully updated.' }
+        format.json { render :show, status: :ok, location: @portfolio_items }
+      else
+        format.html { render :edit }
+        format.json { render json: @portfolio_items.errors, status: :unprocessable_entity }
+      end
+    end
+  end
 end
