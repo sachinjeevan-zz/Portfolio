bd826fa76ada35ee03a5edf4b243181874da3ec1
sachinjeevan <sachinjeevan2506@gmail.com>
2018-03-03 04:54:49 +0000

    validation

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
   resources :portfolios, except: [:show]
   root to: 'pages#home'
-get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
   get 'about', to:'pages#about'
 
   get 'contact', to:'pages#contact'
 
-  resources :blogs
+  resources :blogs do
+  member do
+  get :toggle_status
+  end
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
