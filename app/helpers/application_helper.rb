cb2eb1e8fb3df7448f55c3b05549c52619becf2c
sachinjeevan <sachinjeevan2506@gmail.com>
2018-03-22 04:24:24 +0000

    content tag

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,11 @@ if current_user.is_a?(User)
  (link_to "Login", new_user_session_path )
  end 
 end
+
+def source_helper(layout_name)
+    if session[:source]
+        greeting = "Thanks for visiting me from #{session[:source]}"
+        content_tag(:p, greeting, class: "source-greeting")
+    end
+end
 end
