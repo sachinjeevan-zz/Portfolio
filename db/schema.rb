dd9292180ffc55d703c9e98fed729d348faed60b
sachinjeevan <sachinjeevan2506@gmail.com>
2018-05-17 13:12:28 +0000

    authorization

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180309053914) do
+ActiveRecord::Schema.define(version: 20180426142958) do
 
   create_table "blogs", force: :cascade do |t|
     t.string   "title"
@@ -86,6 +86,7 @@ ActiveRecord::Schema.define(version: 20180309053914) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "roles"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
